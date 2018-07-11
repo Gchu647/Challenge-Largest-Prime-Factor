398372b514ded7df6c79d7f26516176080d74577
George <quicksilver-ga@hotmail.com>
2018-07-11 11:09:38 -1000

    Finish

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,22 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+  // do your work here  
+  while(n%2 === 0) { // Cut out all even numbers
+    n = n/2;
+    primeNumber = n;
+  }
+  
+  let pivot = Math.ceil(Math.sqrt(n));//Factor after even
+  let oddNum = 3;
+  
+  while(oddNum <= pivot) {
+    if(n%oddNum === 0 && n/oddNum !== 1) { //Prevent it from returning 1
+      n = n/oddNum;
+      primeNumber = n;
+    }
+    
+    oddNum = oddNum + 2;
+  }
 
   return primeNumber;
 };
